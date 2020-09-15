7f7bd84a907f2be62760ca7827ec236b3be01d24
Loek Le Blansch <l.leblansch@gmail.com>
2020-09-15 18:42:55 +0200

    update

--- .cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#cbc8c7", "#312322" },
-	[SchemeSel] = { "#cbc8c7", "#c3acaa" },
-	[SchemeOut] = { "#cbc8c7", "#bed5fb" },
-};
--- .cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#312322";
-static const char* selfgcolor   = "#cbc8c7";
-static const char* normbgcolor  = "#8eb8fb";
-static const char* normfgcolor  = "#cbc8c7";
-static const char* urgbgcolor   = "#c3acaa";
-static const char* urgfgcolor   = "#cbc8c7";
