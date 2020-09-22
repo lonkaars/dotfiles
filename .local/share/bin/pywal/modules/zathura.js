cbdbfa7fb9c5c2951eab976ec949ab54aaf0a443
Loek Le Blansch <l.leblansch@gmail.com>
2020-09-22 18:50:29 +0200

    removed zathura transparency

--- .local/share/bin/pywal/modules/zathura.js
+++ .local/share/bin/pywal/modules/zathura.js
@@ -5,7 +5,7 @@ var color = require('color');
 module.exports = colors => {
 	var config = `${fs.readFileSync(path.join(process.env.HOME, "/.config/zathura/zathurarc"))}`;
 
-	config = config.replace(/set\sdefault-bg\s.+/,  `set default-bg ${String(color(colors.special.background).fade(0.08)).replace(/\s/g, '')}`)
+	config = config.replace(/set\sdefault-bg\s.+/,  `set default-bg ${String(color(colors.special.background).fade(0)).replace(/\s/g, '')}`)
 	config = config.replace(/set\srecolor-lightcolor\s.+/, `set recolor-lightcolor ${String(color(colors.special.foreground).fade(1)).replace(/\s/g, '')}`)
 
 	fs.writeFileSync(path.join(process.env.HOME, "/.config/zathura/zathurarc"), config)
