c087875724fb47b66a737f630a63547c45e935cb
lonkaars <l.leblansch@gmail.com>
2020-11-22 12:09:14 +0100

    random wall picker update

--- .local/share/bin/pywal/wall.js
+++ .local/share/bin/pywal/wall.js
@@ -3,20 +3,16 @@ var path = require('path');
 var args = process.argv;
 var exec = require('child_process').execSync;
 
-var dir = Math.random() >= 0.9 ? 1 : 0;
+var collection = fs.readdirSync(path.join(__dirname, "walls/"));
+var walls = Array.from({ length: 2 /* # of screens */ }, () => collection[Math.floor(Math.random() * collection.length)]);
 
-var collections = fs.readdirSync(path.join(__dirname, `/walls/${['mono', 'duo'][dir]}`));
-var collectionName = collections[Math.floor(Math.random() * collections.length)];
-var collection = fs.readdirSync(path.join(__dirname, `/walls/${['mono', 'duo'][dir]}/${collectionName}`));
-var walls = Array.from({ length: Math.abs(dir - 2) }, () => collection[Math.floor(Math.random() * collection.length)]);
+while (walls[0] == walls[1]) walls[1] = collection[Math.floor(Math.random() * collection.length)];
 
-while (walls.length == 2 && walls[0] == walls[1]) walls[1] = collection[Math.floor(Math.random() * collection.length)];
+walls = walls.map(i => path.join(__dirname, `walls/${i}`))
 
-walls = walls.map(i => path.join(__dirname, `/walls/${['mono', 'duo'][dir]}/${collectionName}/${i}`))
-
-var args = ['--bg-fill', walls[0]];
+var args = ['--bg-fill', JSON.stringify(walls[0])];
 if (walls.length == 1) args.push('--no-xinerama');
-if (walls.length == 2) args.push('--bg-fill', walls[1]);
+if (walls.length == 2) args.push('--bg-fill', JSON.stringify(walls[1]));
 
 // jswal(args);
 exec(`feh --no-fehbg ${args.join(' ')}`)
