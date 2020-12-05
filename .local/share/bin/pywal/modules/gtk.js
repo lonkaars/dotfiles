741a935e61705e473a70c3390a790bd31d9ead28
lonkaars <l.leblansch@gmail.com>
2020-12-05 10:49:48 +0100

    removed bulky pywal wrapper

--- .local/share/bin/pywal/modules/gtk.js
+++
@@ -1,5 +0,0 @@
-var exec = require("child_process").execSync;
-
-module.exports = colors => {
-	exec("wpg -i pywal.png /home/$(whoami)/.cache/wal/colors.json && wpg -ns pywal.png", { stdio: "inherit" })
-}
