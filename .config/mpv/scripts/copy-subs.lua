7d11253a69fa759037a29bacfe791df35229e5bf
lonkaars <loek@pipeframe.xyz>
2023-06-03 23:23:49 +0200

    add auto copying to mpv script

--- .config/mpv/scripts/copy-subs.lua
+++ .config/mpv/scripts/copy-subs.lua
@@ -1,9 +1,24 @@
-function copy_sub()
-	sub = mp.get_property("sub-text", "string")
-	if sub and sub ~= '' then
+_G.auto = false
+
+function copy()
+	local sub = mp.get_property("sub-text")
+	if sub then
 		os.execute("echo '" .. sub:gsub('\'', '\'\\\'\'') .. "' | xclip -selection clipboard -i")
 	end
 end
 
-mp.add_key_binding(nil, "copy", copy_sub)
+function autocopy()
+	if _G.auto == false then return end
+	copy()
+end
+
+function toggle_autocopy()
+	_G.auto = not _G.auto
+	mp.osd_message("autocopy " .. (_G.auto and "on" or "off"))
+end
+
+mp.add_key_binding(nil, "copy", copy)
+
+mp.observe_property("sub-text", "string", autocopy)
+mp.add_key_binding(nil, "auto", toggle_autocopy)
 
