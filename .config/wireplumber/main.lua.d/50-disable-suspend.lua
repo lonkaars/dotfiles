be2e6dccf2897c0ba82924bba9da6eb23f5b36ce
lonkaars <loek@pipeframe.xyz>
2024-03-09 22:07:14 +0100

    disable suspend in wireplumber

---
+++ .config/wireplumber/main.lua.d/50-disable-suspend.lua
@@ -0,0 +1,8 @@
+table.insert(alsa_monitor.rules, {
+	matches = {
+		{ { "node.name", "matches", "alsa_input.*" }, },
+		{ { "node.name", "matches", "alsa_output.*" }, },
+	},
+	apply_properties = { ["session.suspend-timeout-seconds"] = 0, },
+});
+
