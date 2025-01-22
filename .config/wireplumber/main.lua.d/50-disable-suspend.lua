ad88ea8a6d55d2d2e7c881d6fb01c0627d6cc822
Loek Le Blansch <loek@pipeframe.xyz>
2025-01-22 22:08:02 +0100

    update wireplumber config

--- .config/wireplumber/main.lua.d/50-disable-suspend.lua
+++
@@ -1,8 +0,0 @@
-table.insert(alsa_monitor.rules, {
-	matches = {
-		{ { "node.name", "matches", "alsa_input.*" }, },
-		{ { "node.name", "matches", "alsa_output.*" }, },
-	},
-	apply_properties = { ["session.suspend-timeout-seconds"] = 0, },
-});
-
