d85d3cde2a62b36ad943a231b244c22a81159680
Loek Le Blansch <loek@pipeframe.xyz>
2026-08-02 19:04:18 +0200

    add bootstrap scripts

---
+++ home/private_dot_ssh/run_once_bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# vim:ft=sh
+
+[ "$(find ~/.ssh -name 'id_*' -maxdepth 1 2>/dev/null | wc -l)" -ne 0 ] && exit 0
+
+echo "generating ssh keys"
+ssh-keygen -q -N "" < /dev/zero 1>/dev/null
+
