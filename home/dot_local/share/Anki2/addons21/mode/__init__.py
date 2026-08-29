a0159a7d3c9eefffdeadd13992da1231581d389e
Loek Le Blansch <loek@pipeframe.xyz>
2026-08-29 16:51:05 +0200

    add XDG_CACHE_HOME fallback value to Anki mode extension

--- home/dot_local/share/Anki2/addons21/mode/__init__.py
+++ home/dot_local/share/Anki2/addons21/mode/__init__.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python3
 from aqt import mw
-from aqt.utils import tr
 from aqt.theme import Theme
-from os import environ
+from os import getenv
 from pathlib import Path
+from sys import platform
 
-THEME_DARK = 2
-THEME_LIGHT = 1
+def set_theme_mode():
+  XDG_CACHE_HOME = Path(getenv('XDG_CACHE_HOME', Path.home().joinpath('.cache')))
+  path = XDG_CACHE_HOME.joinpath('mode/state/mode')
+  with path.open() as file:
+    mode = file.read().strip()
+    if mode == "dark":
+      mw.set_theme(Theme.DARK)
+    if mode == "light":
+      mw.set_theme(Theme.LIGHT)
 
-path = Path(environ['XDG_CACHE_HOME'], 'mode/state/mode')
-with path.open() as file:
-  mode = file.read().strip()
-  if mode == "dark":
-    mw.set_theme(Theme(THEME_DARK))
-  if mode == "light":
-    mw.set_theme(Theme(THEME_LIGHT))
+def set_theme_system():
+  mw.set_theme(Theme.FOLLOW_SYSTEM)
+
+set_theme = {
+  "darwin": set_theme_system,
+  "linux": set_theme_mode,
+}.get(platform, lambda: None)
+set_theme()
 
